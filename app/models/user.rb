f23a18ffe96513afd4f66b8dbd1bff35a033e0a4
Aubrey Masten <mastenaub@gmail.com>
2017-07-24 08:37:18 -0700

    add dependent account creation and destruction to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_one :account
+  has_one :account, dependent: :destroy
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
-  after_save :add_account
+  after_create :add_account
 
   def add_account
     Account.create(user_id: self.id)
