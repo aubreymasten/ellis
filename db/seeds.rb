7fe16372c8a50bf684a5c73c8e792b54018a6a8c
Aubrey Masten <mastenaub@gmail.com>
2017-07-24 14:52:43 -0700

    add story seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,20 @@ class Seed
     seed = Seed.new
 
     seed.generate_admin
+    seed.generate_stories
   end
 
   def generate_admin
     user = User.create email: "admin@gmail.com", password: "password", password_confirmation: "password"
     user.update_attribute :admin, true
+    user.account.update_attribute :name, 'alfred the undying'
+  end
+
+  def generate_stories
+    user = User.first
+    20.times do |index|
+      Story.create(title: Faker::Book.title, description: Faker::Lovecraft.paragraph(4, 6), account_id: user.account.id)
+    end
   end
 
   def self.reset_db
