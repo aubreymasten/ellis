b470a75f0fa32d2d83afc5b8edd63cc09a7c854c
Aubrey Masten <mastenaub@gmail.com>
2017-07-24 13:43:56 -0700

    add short description method for cards

--- app/models/story.rb
+++ app/models/story.rb
@@ -6,4 +6,12 @@ class Story < ApplicationRecord
   scope :most_recent, -> count {
     order(created_at: :desc).limit(count)
   }
+
+  def short_desc
+    if self.description.length > 140
+      self.description.slice(0,140).chomp(".").concat('...')
+    else
+      self.description
+    end
+  end
 end
