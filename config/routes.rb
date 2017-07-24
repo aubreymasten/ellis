3c112994b2d639da57550b7acb613118da8fbe9c
Aubrey Masten <mastenaub@gmail.com>
2017-07-24 10:09:26 -0700

    add stories homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "stories#index"
+
+  resources :accounts do
+    resources :stories
+  end
+
+  resources :stories do
+    resources :scenes
+  end
+
+  resources :scenes do
+    resources :choices
+  end
 end
