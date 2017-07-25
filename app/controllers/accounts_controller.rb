9a710c2e854efee08722dc6b931494f3b3648834
Aubrey Masten <mastenaub@gmail.com>
2017-07-24 17:28:46 -0700

    add public and private scopes to account stories

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,5 +1,7 @@
 class AccountsController < ApplicationController
   def show
     @account = Account.find(params[:id])
+    @private = @account.private_stories
+    @public = @account.public_stories
   end
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,12 @@
 class Account < ApplicationRecord
   belongs_to :user, foreign_key: :user_id, optional: true
   has_many :stories, dependent: :destroy
+
+  def public_stories
+    self.stories.where("public = true")
+  end
+
+  def private_stories
+    self.stories.where("public = false")
+  end
 end
