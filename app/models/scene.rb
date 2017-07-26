024c168247b504a30e43263562db491185811e41
Aubrey Masten <mastenaub@gmail.com>
2017-07-26 10:37:28 -0700

    add shortened description method to scenes

--- app/models/scene.rb
+++ app/models/scene.rb
@@ -2,4 +2,12 @@ class Scene < ApplicationRecord
   belongs_to :story, foreign_key: :story_id, optional: true
   has_many :choices, dependent: :destroy
   validates :title, :text, presence: true
+
+  def get_desc (length)
+    if self.text.length <= length
+      self.text
+    else
+      self.text.slice(0,length-1).chomp(".").concat('...')
+    end
+  end
 end
