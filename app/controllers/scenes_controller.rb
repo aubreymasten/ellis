5f73f44b21553c026927b95311d7ad697b323f56
Aubrey Masten <mastenaub@gmail.com>
2017-07-26 10:31:40 -0700

    add scene creation

--- app/controllers/scenes_controller.rb
+++ app/controllers/scenes_controller.rb
@@ -1,6 +1,19 @@
 class ScenesController < ApplicationController
-  def show_add_scene
-    @story = Story.find(params[:format])
+  def new
+    @story = Story.find(params[:story_id])
     @scene = @story.scenes.new
+  end
+
+  def create
+    @story = Story.find(params[:story_id])
+    @scene =  @story.scenes.new(scene_params)
+    unless @scene.save
+      render partial: 'scenes/edit_error', locals: { scene: @scene }
+    end
+  end
+
+  private
+  def scene_params
+    params.require(:scene).permit(:title, :text)
   end
 end
